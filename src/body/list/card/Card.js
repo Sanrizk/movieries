bf655218453f37831da98d4e0a3dc6540d582b48
ikhsan <ikhsanrizki3@gmail.com>
2023-08-30 20:06:25 +0700

    Fitur DarkMode

--- src/body/list/card/Card.js
+++ src/body/list/card/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card({ imageSrc, title, description, otherInfo }) {
   return(
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden flex mx-48  my-10">
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden flex mx-48 my-10 dark:drop-shadow-[0_35px_35px_rgba(255,255,255,0.25)] dark:bg-stone-600">
 
       {/* Bagian kiri dengan gambar */}
       <div className="w-1/3">
@@ -12,9 +12,9 @@ export default function Card({ imageSrc, title, description, otherInfo }) {
       {/* Bagian kanan dengan teks */}
       <div className="w-2/3 p-4">
         <h2 className="text-xl font-semibold mb-2">{title}</h2>
-        <p className="text-gray-700 mb-4">{description}</p>
+        <p className="mb-4">{description}</p>
         {/* Informasi lain-lain */}
-        <div className="text-sm text-gray-500">
+        <div className="text-sm">
           {otherInfo.map((info, index) => (
             <div key={index} className="mb-1">
               {info}
